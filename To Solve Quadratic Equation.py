56804d1932526ab8fc937d85abc803f3bf63e793
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-09-05 12:48:53 +0530

    To Solve the Quadratic Equation.

---
+++ To Solve Quadratic Equation.py
@@ -0,0 +1,12 @@
+import cmath
+
+a = 2
+b = 5
+c = 3
+
+d = (b**2) - (4*a*c)
+
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
